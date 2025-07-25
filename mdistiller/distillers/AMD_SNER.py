afd355b48500f7becada9e664f77ecc1f4074604
sunj <sunj@mobiltech.io>
2025-07-25 17:49:47 +0900

    SNER Pipeline modification

--- mdistiller/distillers/AMD_SNER.py
+++ mdistiller/distillers/AMD_SNER.py
@@ -65,36 +65,38 @@ class AMD_SNER(Distiller):
         with torch.no_grad():
             _, feature_teacher = self.teacher.forward_wohead(image)
             
-        loss_feat, loss_outlier, loss_info = 0.0, 0.0, 0.0
+        loss_feat, loss_outlier, loss_info = 0.0, 0.0, 0.0    
         for m_l_stu, m_l in zip(self.m_layers_stu, self.m_layers):
-            f_s = feature_student["feats"][m_l_stu]
-            f_t = feature_teacher["feats"][m_l] # y_base
             
-            # for SNER
-            f_t_before = feature_teacher["feats"][m_l-1] # get before feature
-            cls, patch = f_t_before[:, :1], f_t_before[:, 1:]
-            patch_mod = self.sner_dict[f"sner_{m_l:03d}"](patch)
-            mod = torch.cat((cls, patch_mod), 1)
-            f_t_sner = self.teacher.blocks[m_l].forward(mod) # y_hat
+            f_s = feature_student["feats"][m_l_stu ]# F_S^l
+            f_t = feature_teacher["feats"][m_l]  # F_T^l
             
-            distill_f_t = f_t_sner.clone()
+            cls, patch = f_t[:, :1], f_t[:, 1:]
+            patch_sner = self.sner_dict[f"sner_{m_l:03d}"](patch)
+            f_t_sner = torch.cat((cls, patch_sner), 1)  # \hat{F}_T^l
+
+            # 3.1. Knowledge Distillation Loss (L_KD)
             proj_f_s = self.adapter_dict[f"adapter_{m_l_stu:03d}"](f_s)
-            
-            # feature loss
-            loss_feat = loss_feat + F.mse_loss(proj_f_s, distill_f_t)
-            # info loss
-            cosine_sim = F.cosine_similarity(f_t_sner, f_t, dim=-1) 
-            loss_info = loss_info + (1.0 - cosine_sim).mean() 
-            # outlier loss
-            norms = patch_mod.norm(dim=-1).detach()
-            q_threshold = torch.quantile(norms, self.outlier_q, dim=1, keepdim=True)
+            loss_feat = loss_feat + F.mse_loss(proj_f_s, f_t_sner) # \hat{F}_T^l - F_S^l
+
+            # 3.2. Outlier Suppression Loss (L_outlier)
+            norms = f_t_sner[:, 1:].norm(dim=-1)
+            q_threshold = torch.quantile(norms, self.outlier_q, dim=1, keepdim=True).detach()
             outlier_norms = norms[norms > q_threshold]
             
             if outlier_norms.numel() > 0:
                 target_norms = q_threshold.expand_as(norms)[norms > q_threshold]
                 loss_outlier = loss_outlier + F.mse_loss(outlier_norms, target_norms)
             else:
-                loss_outlier = loss_outlier + torch.tensor(0.0, device=dev)
+                loss_outlier = loss_outlier + torch.tensor(0.0, device=f_s.device)
+
+            # 3.3. Information Preservation Loss (L_info)
+            with torch.no_grad():
+                f_t_next = self.teacher.blocks[m_l + 1].forward(f_t) # F_T^{l+1}
+            
+            f_t_sner_next = self.teacher.blocks[m_l + 1].forward(f_t_sner) # \hat{F}_T^{l+1}
+            cosine_sim = F.cosine_similarity(f_t_sner_next, f_t_next, dim=-1) # \hat{F}_T^{l+1} - F_T^{l+1}
+            loss_info = loss_info + (1.0 - cosine_sim).mean()
             
         loss_feat = self.feat_loss_weight * loss_feat / len(self.m_layers) 
         loss_info = self.info_loss_weight * loss_info / len(self.m_layers) 
