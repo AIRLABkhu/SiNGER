7dc18296c7836e01828049e14d090f4ad38b0768
nda111 <geunhyeok@khu.ac.kr>
2025-08-08 08:50:00 +0000

    Refactor: Eliminated redundant codes.
    On branch dev/fitnet-multilayer
    Your branch is up to date with 'origin/dev/fitnet-multilayer'.
    
    Changes to be committed:
    	modified:   mdistiller/distillers/FitViT.py

--- mdistiller/distillers/FitViT.py
+++ mdistiller/distillers/FitViT.py
@@ -5,19 +5,6 @@ import torch.nn.functional as F
 from ._base import Distiller
 from ._common import get_feat_shapes, SimpleAdapter, compute_mapped_layers
 
-@torch.no_grad()
-def modified_zscore(data: torch.Tensor, threshold=3.5):
-    '''
-    data: batch, spatial, channel
-    '''
-    x = data.norm(dim=-1)  # batch, spatial
-    median = torch.median(x, dim=1, keepdim=True).values
-    mad = torch.median(torch.abs(x - median), dim=1, keepdim=True).values
-
-    modified_z = 0.6745 * (x - median) / mad
-    outlier_mask = torch.abs(modified_z) > threshold
-    return outlier_mask
-
 
 class FitViT(Distiller):
     """from FitNets: Hints for Thin Deep Nets"""
@@ -55,28 +42,12 @@ class FitViT(Distiller):
         # losses
         loss_ce = self.ce_loss_weight * F.cross_entropy(logits_student, target)
         f_s = self.adapter(feature_student["feats"][self.hint_layer_stu])
-
-        if self.af_enabled:
-            f_t = feature_teacher["feats"][self.hint_layer]
-            match self.af_type:
-                case 'zscore':
-                    outlier_mask = modified_zscore(f_t, threshold=self.af_threshold)
-                case _:
-                    raise NotImplementedError(self.af_type)
-            f_t[outlier_mask] = torch.nan
-            f_s[outlier_mask] = torch.nan
-            loss_feat = self.feat_loss_weight * torch.square(f_s - f_t).nanmean()
-
-        else:
-            loss_feat = self.feat_loss_weight * F.mse_loss(
-                f_s, feature_teacher["feats"][self.hint_layer]
-            )
+        loss_feat = self.feat_loss_weight * F.mse_loss(
+            f_s, feature_teacher["feats"][self.hint_layer]
+        )
 
         losses_dict = {
             "loss_ce": loss_ce,
             "loss_kd": loss_feat,
         }
         return logits_student, losses_dict
-
-    # def 
-
