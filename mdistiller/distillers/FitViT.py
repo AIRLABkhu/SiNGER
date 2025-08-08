95338fd256c3df33c1126f65be982fb74c0042f3
nda111 <geunhyeok@khu.ac.kr>
2025-08-08 09:14:26 +0000

    Feature: Multi-layer distillation of FitNet impl.
    On branch dev/fitnet-multilayer
    Your branch is ahead of 'origin/dev/fitnet-multilayer' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   configs/imagenet/vit/fitvit.yaml
    	modified:   mdistiller/distillers/FitViT.py
    	modified:   mdistiller/engine/cfg.py

--- mdistiller/distillers/FitViT.py
+++ mdistiller/distillers/FitViT.py
@@ -13,38 +13,43 @@ class FitViT(Distiller):
         super(FitViT, self).__init__(student, teacher)
         self.ce_loss_weight = cfg.FITNET.LOSS.CE_WEIGHT
         self.feat_loss_weight = cfg.FITNET.LOSS.FEAT_WEIGHT
-        self.hint_layer = cfg.FITNET.HINT_LAYER
-        self.hint_layer_stu = compute_mapped_layers([self.hint_layer], self.teacher, self.student)[0]
+        self.hint_layer = [*cfg.FITVIT.M_LAYERS, len(self.teacher.get_layers()) - 1]
+        self.hint_layer_stu = compute_mapped_layers(self.hint_layer, self.teacher, self.student)
         feat_s_shapes, feat_t_shapes = get_feat_shapes(
             self.student, self.teacher, cfg.FITNET.INPUT_SIZE
         )
-        self.adapter = SimpleAdapter(
-            feat_s_shapes[self.hint_layer_stu][-1], feat_t_shapes[self.hint_layer][-1]
-        )
+        self.adapters = nn.ModuleDict({
+            f"adapter_{m_l_stu:03d}": SimpleAdapter(feat_s_shapes[m_l_stu][-1], feat_t_shapes[m_l][-1])
+            for m_l_stu, m_l in zip(self.hint_layer_stu, self.hint_layer)
+        })
 
         self.af_enabled = cfg.FITNET.AF.ENABLE
         self.af_type = cfg.FITNET.AF.CRITERIA.TYPE
         self.af_threshold = cfg.FITNET.AF.CRITERIA.THRES
 
     def get_learnable_parameters(self):
-        return super().get_learnable_parameters() + list(self.adapter.parameters())
+        return super().get_learnable_parameters() + list(self.adapters.parameters())
 
     def get_extra_parameters(self):
         num_p = 0
-        for p in self.adapter.parameters():
+        for p in self.adapters.parameters():
             num_p += p.numel()
         return num_p
 
     def forward_train(self, image, target, **kwargs):
         logits_student, feature_student = self.student(image)
         with torch.no_grad():
-            feature_teacher = self.teacher.forward_partial(image, self.hint_layer)
+            _, feature_teacher = self.teacher.forward_wohead(image)
+            
         # losses
         loss_ce = self.ce_loss_weight * F.cross_entropy(logits_student, target)
-        f_s = self.adapter(feature_student["feats"][self.hint_layer_stu])
-        loss_feat = self.feat_loss_weight * F.mse_loss(
-            f_s, feature_teacher["feats"][self.hint_layer]
-        )
+        loss_feat: torch.Tensor = 0.0
+        for m_l_stu, m_l in zip(self.hint_layer_stu, self.hint_layer):
+            adapter: SimpleAdapter = self.adapters[f"adapter_{m_l_stu:03d}"]
+            f_s = feature_student['feats'][m_l_stu]
+            f_t = feature_teacher['feats'][m_l]
+            loss_feat = loss_feat + F.mse_loss(adapter.forward(f_s), f_t)
+        loss_feat = self.feat_loss_weight * loss_feat
 
         losses_dict = {
             "loss_ce": loss_ce,
